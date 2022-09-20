814f3464a60f36bde3bdd6ec9c809296cf4103f4
b3mlo <rahul.suthar@dal.ca>
2022-09-20 14:18:29 -0300

    Final edit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,9 +3,9 @@ import math
 for i in range (1,101):
     if(i % 3 == 0 and i % 5 == 0):
         print("FiizBuzz")
-    if(i % 3 == 0):
+    elif(i % 3 == 0):
         print("Fizz")
-    if(i % 5 == 0):
+    elif(i % 5 == 0):
         print("Buzz")
     else:
         print(i)
